213a21cbc4993099f0097f9faf66718185d71434
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2019-10-29 11:14:27 +0200

    api news

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    db:' mongodb+srv://jbej:Ud1cV7eS4iB1j3qa@justynabejgrowicz-ov4um.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
+
+// Ud1cV7eS4iB1j3qa
